aa62d01f52154af3c44bdca8455ddc9163433f79
hnivq1504 <vinhtlq1504@gmail.com>
2023-04-21 06:50:34 +0700

    scraper CVE news v0.1

--- app.js
+++ app.js
@@ -1,10 +1,65 @@
-const {Bot} = require("./bot.js");
+const puppeteer = require('puppeteer');
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
+const fs = require('fs/promises');
 
-const token = "6216004222:AAHo7A_DoK5e6sCdfBHp2VpwOn_H-Hg9Og8";
-const chat_id = "-901299271";
+async function getLinks(url, hint,) {
+    const browser = await puppeteer.launch({ executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe" });
+    const page = await browser.newPage();
+    await page.goto(url);
 
-const bot = new Bot(token, chat_id);
+    const links = await page.evaluate(() => {
+        const anchorTags = Array.from(document.querySelectorAll('a'));
+        const filterAnchorTags = anchorTags.filter(tag => {
+            return tag.href && tag.href.includes('/Media/News/item/news')
+        });
+        const url = filterAnchorTags.map(tag => tag.href);
+        return url;
+    });
+    
+    await browser.close();
+    return links;
+}
 
-bot.send_message("test send message from NodeJS code!!!")
-.then(() => console.log("Done!!!"))
-.catch((error) => console.error("Error sending message: ", error))
+async function getDetail(url) {
+    const browser = await puppeteer.launch({ executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe" });
+    const page = await browser.newPage();
+    await page.goto(url);
+    
+    const author = null;
+    const tag = null;
+    const linkWebsite = null;
+    const linkArticle = url;
+    const title = await page.$eval('h1.title', element => element.textContent);
+    const published = await page.$eval('time', element => element.textContent);
+    
+    await browser.close();
+
+    const article = {
+        "Website": linkWebsite,
+        "Link": linkArticle,
+        "Title": title,
+        "Published": published,
+        "Author": author,
+        "Tag": tag,
+    };
+    return article;
+}
+
+async function start() {
+    const url = "https://www.cve.org/Media/News/AllNews";
+    const hint = '/Media/News/item/news';
+    const links = await getLinks(url, hint);
+
+    const articles = [];
+    for (const link of links){
+        const article = await getDetail(link);
+        articles.push(article);
+    }
+
+    const data = JSON.stringify(articles, null, 2);
+    await fs.writeFile('CVE News.json', data);
+    console.log(typeof (links))
+};
+
+start();
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer-core");
 
 const url = "https://www.cve.org/Media/News/AllNews";
-const excutePath = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
+const excutePath = "C:/Program Files/Google/Chrome/Application/chrome.exe";
 
 async function test() {
     const browser = await puppeteer.launch({
@@ -14,7 +14,21 @@ async function test() {
     });
     const page = await browser.newPage();
     await page.goto(url);
-    const link = await page.$eval('a[href')
+
+    const links = await page.evaluate(() => {
+        const anchorTags = Array.from(document.querySelectorAll('a'));
+        const filterAnchorTags = anchorTags.filter(tag => {
+            return tag.href && tag.href.includes('/Media/News/item/news');
+        });
+        const url = filterAnchorTags.map(tag => tag.href);
+        return url;
+    });
+    
+    console.log(links)
+
+
+
+    // const link = await page.$eval('a[href')
     // const [element] = await page.$x("/html/body/div/div/div/div/div[1]/main/div/div[1]/article/div/div/h2/a");
     // if (element) {
     //     await element.click();
@@ -27,3 +41,46 @@ async function test() {
 }
 
 test()
+
+const puppeteer = require('puppeteer');
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
+const fs = require('fs/promises');
+
+async function getLinks(url, hint,) {
+    const browser = await puppeteer.launch({ executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe" });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const links = await page.evaluate(() => {
+        const anchorTags = Array.from(document.querySelectorAll('a'));
+        const filterAnchorTags = anchorTags.filter(tag => {
+            return tag.href && tag.href.includes('/Media/News/item/news')
+        });
+        const url = filterAnchorTags.map(tag => tag.href);
+        return url;
+    });
+
+    return links;
+}
+
+async function getDetail(url) {
+    const browser = await puppeteer.launch({ executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe" });
+    const page = await browser.newPage();
+    await page.goto(url);
+    
+    const title = await page.title();
+    await browser.close();
+    console.log(title);
+}
+
+async function start() {
+    const url = "https://www.cve.org/Media/News/AllNews";
+    const hint = '/Media/News/item/news';
+    const links = await getLinks(url, hint);
+    await fs.writeFile('CVE News.txt', links.join("\r\n"));
+    console.log(typeof (links))
+    await browser.close();
+};
+
+getDetail("https://www.cve.org/Media/News/item/news/2023/04/11/Halborn-Added-as-CNA");
